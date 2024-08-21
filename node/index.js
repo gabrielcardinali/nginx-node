d84ac4acfaeb9b94fb3870c3cb490f9b1b1be92d
Gabriel Cardinali <gabrielccardinalif@gmail.com>
2024-08-21 10:23:52 -0300

    feat: lista de nomes cadastrados exibida na página

---
+++ node/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 8080
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config);
+
+    const sqlInsert = `INSERT INTO people(name) values('Gabriel')`;
+    connection.query(sqlInsert, (err) => {
+        if (err) throw err;
+
+        const sqlSelect = `SELECT name FROM people`;
+        connection.query(sqlSelect, (err, results) => {
+            if (err) throw err;
+
+            let response = '<h1>Full Cycle Rocks!</h1>';
+            response += '<ul>';
+            results.forEach(row => {
+                response += `<li>${row.name}</li>`;
+            });
+            response += '</ul>';
+
+            res.send(response);
+            connection.end();
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port);
+});
